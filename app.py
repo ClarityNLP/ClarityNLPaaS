6a6c429ab8e9334adcd58c451e8c7caf88c145f1
cjamadagni <chirag.jamadagni@gmail.com>
2018-10-14 04:21:27 -0400

    API blueeprint

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Welcome to ClarityNLPaaS"
+
+
+"""
+API for Value Extraction
+"""
+@app.route("/valueExtraction", methods=['POST', 'GET'])
+def valueExtraction():
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+    else:
+        return Response('This API supports only POST & GET requests', status=400, mimetype='application/json')
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
