c30dff1a3484139a7c09d107f2a5b3fed535789f
cjamadagni <chirag.jamadagni@gmail.com>
2018-10-14 14:38:07 -0400

    API blueprint

---
+++ util.py
@@ -0,0 +1,25 @@
+import configparser
+from os import getenv, environ, path
+
+SCRIPT_DIR = path.dirname(__file__)
+config = configparser.RawConfigParser()
+config.read(path.join(SCRIPT_DIR, 'project.cfg'))
+
+
+def read_property(env_name, config_tuple):
+    property_name = ''
+    if getenv(env_name):
+        property_name = environ.get(env_name)
+    else:
+        try:
+            property_name = config.get(config_tuple[0], config_tuple[1])
+        except Exception as ex:
+            print(ex)
+    return property_name
+
+# Solr
+solr_url = read_property('NLP_SOLR_URL', ('solr', 'url'))
+
+# Mongo
+mongo_host = read_property('NLP_MONGO_HOSTNAME', ('mongo', 'host'))
+mongo_port = int(read_property('NLP_MONGO_CONTAINER_PORT', ('mongo', 'port')))
--- worker.py
+++ worker.py
@@ -2,7 +2,9 @@ import json
 import string
 import requests
 from flask import Response
+import util
 
+# Input validation
 def validateInput(data):
     if 'userId' not in data or 'report' not in data:
         return False
@@ -14,9 +16,56 @@ def validateInput(data):
     return True
 
 
+def uploadReport(data):
+    # Constructing Solr request
+    url = util.solr_url + '/update?commit=true'
+
+    headers = {
+        'Content-type': 'application/json'
+    }
+
+    data = {
+        "report_type": data['report']['report_type'],
+        "id": data['userId'],
+        "report_id": data['userId'],
+        "source": data['userId'],
+        "report_date": data['report']['report_date'],
+        "subject": data['userId'],
+        "report_text": data['report']['report_text']
+    }
+
+    print(data)
+
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
+
+def getNLPQL(data):
+    # TODO: Write the code to extract the required NLPQL and enter data
+    pass
+
+def hasActiveJob(data):
+    # TODO: Query Mongo and see if the user has any current active job
+    # If active job is present return
+    return False
+
+
 def worker(data):
+
+    # Checking for active Job
+    if hasActiveJob() == True:
+        return Response(json.dumps({'message': 'You currently have an active job. Only one active job allowed'}), status=200, mimetype='application/json')
+
     # Validating the input object
     if validateInput(data) == False:
         return Response(json.dumps({'message': 'Input JSON object is invalid'}), status=400, mimetype='application/json')
-    else:
-        return Response(json.dumps({'message': 'OK'}), status=200, mimetype='application/json')
+
+    # Uploading report to Solr
+    if uploadReport(data) == False:
+        return Response(json.dumps({'message': 'Could not upload report to Solr'}), status=500, mimetype='application/json')
+
+    return Response(json.dumps({'message': 'OK'}), status=200, mimetype='application/json')
