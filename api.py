35dea91093972bc5e6d29ca5c255b5667413e333
Andy Stevens <andrew.stevens@gtri.gatech.edu>
2022-08-23 15:10:56 -0400

    Update api.py

--- api.py
+++ api.py
@@ -50,7 +50,5 @@ def run_nlpql(nlpql_library: str, post_body: RunNLPQLPostBody):
     '''
     Runs NLPQL library given in path against patient and documents given in post body
     '''
-    temp_output = run_job(nlpql_library, post_body)
-    logger.info('Run NLPQL Output:')
-    logger.info(temp_output)
-    return temp_output
+
+    return run_job(nlpql_library, post_body)
